19ba96d325dd2245cf327b765ad9181bae0c7305
seifelsayed07 <saifelsayedelatfy@gmail.com>
2026-08-27 04:31:37 +0300

    new edit

--- app.js
+++ app.js
@@ -5,14 +5,21 @@ console.log("MY New CODE")
 1 2 3 4 5
 */
 console.log("read this line from left to right")
-var Myname = "seif elsayed "
-var Myage = 19
-var Mycountry = "Egypt"
-var Mycity = "menofia"
-var Mycontact = "01093965828"
-var Myemail = "saifelsayedelatfy@gmail.com"
-console.log ("My name is " + Myname + " , I am "+ Myage + " Years old , I live in " + Mycountry + " , My city is " + Mycity + " , MY Contact Number is " + Mycontact + " and My email is " + Myemail)
-var defineYourself = "I am a student at Menofia University"
+var myName = "seif elsayed "
+var myAge = 19
+var myCountry = "Egypt"
+var myCity = "menofia"
+var myContact = "01093965828"
+var myEmail = "saifelsayedelatfy@gmail.com"
+console.log ("My name is " + myName)
+console.log ("My age is " + myAge)
+console.log ("My country is " + myCountry)
+console.log ("My city is " + myCity)
+console.log ("My contact is " + myContact)
+console.log ("My email is " + myEmail)
+let defineYourself = "I am a student at Menofia University"
 console.log(defineYourself)
-let var1 = "Mohamed salah my favorite player"
+let var1 = "Mohamed salah"
 console.log("My favourite player is " + var1)
+let var2 = "Electrical engineering my specialization"
+console.log("My specialization is " + var2)
