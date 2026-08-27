1dc28c67c737bbb24030249ac9781d2785b24113
seifelsayed07 <saifelsayedelatfy@gmail.com>
2026-08-27 04:25:02 +0300

    added my info

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ console.log("MY New CODE")
 1 2 3 4 5
 */
 console.log("read this line from left to right")
+var Myname = "seif elsayed "
+var Myage = 19
+var Mycountry = "Egypt"
+var Mycity = "menofia"
+var Mycontact = "01093965828"
+var Myemail = "saifelsayedelatfy@gmail.com"
+console.log ("My name is " + Myname + " , I am "+ Myage + " Years old , I live in " + Mycountry + " , My city is " + Mycity + " , MY Contact Number is " + Mycontact + " and My email is " + Myemail)
+var defineYourself = "I am a student at Menofia University"
+console.log(defineYourself)
+let var1 = "Mohamed salah my favorite player"
+console.log("My favourite player is " + var1)
