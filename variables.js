039153c6f3f3c711126614b7614814ae927e8780
seifelsayed07 <saifelsayedelatfy@gmail.com>
2026-08-28 07:53:23 +0300

    seconed day

--- variables.js
+++ variables.js
@@ -7,4 +7,33 @@ console.log(c)
 a=c
 b=a+c
 console.log(a+b+c)
-
+const quotient = 18/4
+console.log(quotient)
+let myVar = 92
+myVar++
+console.log(myVar)
+myVar--
+myVar--
+myVar--
+console.log(myVar)
+const quotient2 = 2.0/2.0
+console.log(quotient2)
+const var1 = 25/2
+console.log(var1)
+const var2 = 25%2
+console.log(var2)
+let x = 27
+let y = 153
+let z = 1234
+x+= 13
+y-= 23
+z*= 2
+let d = 32
+d/= 4
+console.log(x)
+console.log(y)
+console.log(z)
+console.log(d)
+// ++ mean +1 -- mean -1
+/*new mulit line
+commnt*/
